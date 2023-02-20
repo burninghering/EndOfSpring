0b60785c4e0bfef0ee07c6a522dd73f548272ac1
kimhyerin <37091602+burninghering@users.noreply.github.com>
2023-02-21 03:36:51 +0900

    MVC pattern

--- ch2/YoilTeller.java
+++ ch2/YoilTeller.java
@@ -15,7 +15,10 @@ public class YoilTeller {
 	
 	@RequestMapping("/getYoil")
 	public  void main(HttpServletRequest request,HttpServletResponse response) throws IOException {
-      String year = request.getParameter("year");
+      
+		
+	// 1.입력 
+	  String year = request.getParameter("year");
       String month = request.getParameter("month");
       String day = request.getParameter("day");
 
@@ -23,12 +26,14 @@ public class YoilTeller {
       int mm = Integer.parseInt(month);
       int dd = Integer.parseInt(day);
       
+    // 2.처리
       Calendar cal = Calendar.getInstance();
       cal.set(yyyy, mm - 1, dd);
       
       int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
       char yoil = " 일월화수목금토".charAt(dayOfWeek);   // 일요일:1, 월요일:2, ... 
      
+    // 3.출력
       response.setContentType("text/html");
       response.setCharacterEncoding("utf-8");
       PrintWriter out = response.getWriter(); //response 객체에서 브라우저로의 출력 스트림을 얻는다
