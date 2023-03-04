c49c4cf6ef3c70bbb38687ad45ef2327361ab643
kimhyerin <37091602+burninghering@users.noreply.github.com>
2023-03-04 20:19:19 +0900

    move boardlist

--- ch2/src/main/java/com/fastcampus/ch2/BoardController.java
+++ ch2/src/main/java/com/fastcampus/ch2/BoardController.java
@@ -1,5 +1,8 @@
 package com.fastcampus.ch2;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
 
 	@GetMapping("/list")
-	public String list() {
-		return "boardList";
+	public String list(HttpServletRequest request) {
+		if(!loginCheck(request)) { //loginCheck가 true가 아니라면
+			return "redirect:/login/login"; //login을 안했으면 login 화면으로 이동 
+		}
+		return "boardList"; //login을 했으면 게시판 화면으로 이동
+	}
+
+	private boolean loginCheck(HttpServletRequest request) {
+		//1.세션을 얻어서
+		HttpSession session = request.getSession();
+		
+		//2.세션에 id가 있는지 확인
+//		if(session.getAttribute("id")!=null)
+//			return true;
+//		else
+//			return false;
+		
+		return session.getAttribute("id")!=null; //null이 아니면 true, null이면 false
 	}
 }
